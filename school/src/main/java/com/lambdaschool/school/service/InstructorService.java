81ef2fc0c60544285a3e7c6e1e9847419c3b3c6f
Nicholas Pung <nicholaspung>
2019-07-22 15:48:36 -0700

    Added instructor endpoint to finish 1 stretch goal

--- school/src/main/java/com/lambdaschool/school/service/InstructorService.java
+++ school/src/main/java/com/lambdaschool/school/service/InstructorService.java
@@ -1,5 +1,12 @@
 package com.lambdaschool.school.service;
 
+import com.lambdaschool.school.model.Instructor;
+
 public interface InstructorService
 {
+    Instructor save(Instructor instructor);
+
+    Instructor update(Instructor instructor, long id);
+
+    void delete(long id);
 }
--- school/src/main/java/com/lambdaschool/school/service/InstructorServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/InstructorServiceImpl.java
@@ -1,12 +1,45 @@
 package com.lambdaschool.school.service;
 
+import com.lambdaschool.school.exceptions.ResourceNotFoundException;
+import com.lambdaschool.school.model.Instructor;
 import com.lambdaschool.school.repository.InstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service(value = "instructorService")
 public class InstructorServiceImpl implements InstructorService
 {
     @Autowired
     private InstructorRepository instructrepos;
+
+    @Override
+    public Instructor save(Instructor instructor) {
+        return instructrepos.save(instructor);
+    }
+
+    @Override
+    public Instructor update(Instructor instructor, long id) {
+        Instructor newInstructor = instructrepos.findById(id).orElseThrow(() -> new ResourceNotFoundException(Long.toString(id)));
+
+        if (instructor.getInstructname() != null) {
+            newInstructor.setInstructname(instructor.getInstructname());
+        }
+
+        if (instructor.getCourses() != null) {
+            newInstructor.setCourses(instructor.getCourses());
+        }
+
+        return instructrepos.save(newInstructor);
+    }
+
+    @Transactional
+    @Override
+    public void delete(long id) {
+        if (instructrepos.findById(id).isPresent()) {
+            instructrepos.deleteById(id);
+        } else {
+            throw new ResourceNotFoundException(Long.toString(id));
+        }
+    }
 }
